f78aaea5b25ec6dbb1e4dfe03c878d456a6a26b2
Surface Bob <joshua.eygenraam@ryerson.ca>
2022-07-31 17:21:48 -0400

    annotated server.js

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,12 @@ import express from "express"
 import cors from "cors"
 import restaurants from "./api/restaurants.route.js"
 
-const app = express()
+const app = express() // server
 
-app.use(cors())
-app.use(express.json())
+app.use(cors()) // middlemen
+app.use(express.json()) 
 
-app.use("/api/v1/restaurants", restaurants)
-app.use("*", (req, res) => res.status(404).json({ error: "not found"}))
+app.use("/api/v1/restaurants", restaurants) // most routes in other file, this url is what people will go to
+app.use("*", (req, res) => res.status(404).json({ error: "not found"})) // if they go elsewhere then error
 
-export default app
+export default app // export to file that will interact with database
